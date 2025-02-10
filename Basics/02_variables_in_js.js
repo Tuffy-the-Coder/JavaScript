74edba7fd985586af1e119365015d8b465a52283
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-10 21:52:02 +0530

    Day 1

---
+++ Basics/02_variables_in_js.js
@@ -0,0 +1,8 @@
+var name = "Tuffy"; // old way not recommended.
+let age = 20; // value can be changed later but variable.
+const favmovie = "Badass Ravikumnar"; // constant value.
+console.log(name);
+console.log(age);
+console.log(favmovie);
+age = age+1; // updating value
+console.log(age);
