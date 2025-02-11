a11c527ed08fd54aae8fd9649dbf16327eb80ad9
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-11 21:27:01 +0530

    day 2

--- Basics/04_objects.js
+++ Basics/04_objects.js
@@ -1,7 +1,13 @@
 const student = {
     fullName : "Anurag",
     age : 20,
-    Cgpa : 5.5,
+    cgpa : 5.5,
 };
+console.log(student);
+// accessing individually 
 console.log(student.fullName);  
 console.log(student["age"]); 
+// Modifying 
+student["age"] = 21;
+student.cgpa = 6.6;
+console.log(student);
