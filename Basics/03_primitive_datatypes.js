ded715b00de19dc2130f4603ada3e6fabe14901e
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-11 20:05:50 +0530

    Day 2
    7 primitive datatypes

---
+++ Basics/03_primitive_datatypes.js
@@ -0,0 +1,9 @@
+let a = null; // null datatype
+let b = 12; // number datatype
+let c = true; // boolean datatype
+let d = BigInt("787"); // bigint datatype
+let e = "Anurag"; // string datatype
+let f = Symbol("unique"); // symbol datatype
+let g; // undefinied datatype
+console.log(a, b, c ,d ,e ,f);
+console.log(typeof f)
