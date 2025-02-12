ea09c5b6ef8134d11bfa4c8dca7bd78e69e50c89
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-12 21:57:21 +0530

    day 3

---
+++ Basics/06_operators.js
@@ -0,0 +1,35 @@
+// Arthmetic operators
+let a = 5;
+let b = 4;
+console.log("a = ",a,"b = ",b);
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);
+console.log("a**b = ",a**b);
+console.log("a++ = ",a++);
+console.log("++b = ",++b);
+console.log("a-- = ",a--);
+console.log("--b = ",--b);
+// Assignment operators
+let c = 3;
+console.log("c = 2",c);
+console.log(c+=2);
+console.log(c-=2);
+console.log(c*=2);
+console.log(c/=2);
+console.log(c**=2);
+console.log(c%=2);
+// comparison operators
+let d = 8;
+console.log("c == d",c == d);
+console.log("c != d",c != d);
+console.log("c === d",c === d);
+console.log("c !== d",c !== d);
+console.log("c >= d",c >= d);
+console.log("c <= d",c <= d);
+// logical operator
+console.log(c<d && c==d);
+console.log(c<d || c==d);
+console.log(!true);
