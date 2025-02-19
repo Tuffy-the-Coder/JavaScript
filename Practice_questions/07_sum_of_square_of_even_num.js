6921fa6f26f15de2beb4a55eb11847392801e9d6
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-20 00:36:32 +0530

    day 9

--- Practice_questions/07_sum_of_square_of_even_num.js
+++ Practice_questions/07_sum_of_square_of_even_num.js
@@ -6,5 +6,5 @@ for (let i = 0; i <= 100; i++) {
 let sum_of_even = numbers
     .filter((element) => { return element % 2 == 0 })
     .map((element) => { return element * element })
-    .reduce((e1, e2) => { return e1 + e2 },0);
+    .reduce((e1, e2) => { return e1 + e2 }, 0);
 console.log(`Sum of squares of all even numbers btw 0-100 is ${sum_of_even}`);
