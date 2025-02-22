124b19090408957075179b6a23adc683ac2bb673
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-22 21:53:48 +0530

    day 12

---
+++ Practice_questions/09_random_password_generator.js
@@ -0,0 +1,9 @@
+// Create a random password generator using random characters.
+let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+const pswdSize = 12;
+let password = "";
+for (i = 0; i < pswdSize; i++) {
+    let index = Math.floor(Math.random() * letters.length);
+    password += letters[index];
+};
+console.log(password);
