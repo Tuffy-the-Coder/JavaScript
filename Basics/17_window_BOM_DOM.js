a54894504cbd739dd9c0c70573140144f23a2ffc
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-02-25 21:12:10 +0530

    day 14

---
+++ Basics/17_window_BOM_DOM.js
@@ -0,0 +1,6 @@
+window.location.href = "https://www.google.com";
+console.log(document.title);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+document.body.style.backgroundColor = "lightblue";
+
