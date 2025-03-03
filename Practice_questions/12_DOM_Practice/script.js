832b6d847c062c689362851fb0c602421c7b22e4
Tuffy-the-Coder <rawat123anurag@gmail.com>
2025-03-03 21:03:43 +0530

    day 19

---
+++ Practice_questions/12_DOM_Practice/script.js
@@ -0,0 +1,7 @@
+let button = document.createElement("button");
+button.innerText = "Click me!"
+button.style.backgroundColor = "red";
+button.style.color = "white";
+document.querySelector("body").prepend(button);
+
+document.querySelector(".para").classList.add("newPara");
